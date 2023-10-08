d8b02ba843fd263f84c7f6f806a14e0c46a8443d
SHADOW <505029990@qq.com>
2023-10-08 23:51:52 +0800

    fix: compatible with md4

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,6 +29,22 @@ const postcssNormalize = require("postcss-normalize");
 
 const appPackageJson = require(paths.appPackageJson);
 
+const crypto = require("crypto");
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash("md4");
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === "md4" ? "md5" : alg, opts);
+  };
+}
+
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
